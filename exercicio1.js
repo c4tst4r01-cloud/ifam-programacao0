3fa916f957262d529af59ea6b662dad404cb815a
c4tst4r01-cloud <c4tst4r01@gmail.com>
2025-11-06 01:09:44 -0400

    Update exercicio1.js with new content

--- exercicio1.js
+++ exercicio1.js
@@ -1,11 +1,36 @@
-let n = parseInt(prompt("Quantos números você quer digitar?"));
-let numeros = [];
+(function() {
+  // Solicita a quantidade de números a serem digitados e valida a entrada
+  let n;
+  while (true) {
+    const inputN = prompt("Quantos números você quer digitar?");
+    if (inputN === null) {
+      document.write("Entrada cancelada pelo usuário.");
+      return;
+    }
+    n = parseInt(inputN, 10);
+    if (Number.isInteger(n) && n > 0) break;
+    alert("Por favor, insira um número inteiro positivo.");
+  }
 
-for (let i = 0; i < n; i++) {
-  numeros[i] = parseInt(prompt(`Digite o número ${i + 1}:`));
-}
+  // Lê os números, validando cada entrada
+  const numeros = [];
+  for (let i = 0; i < n; i++) {
+    while (true) {
+      const input = prompt(`Digite o número ${i + 1}:`);
+      if (input === null) {
+        document.write("Entrada cancelada pelo usuário.");
+        return;
+      }
+      const num = Number(input);
+      if (!Number.isNaN(num)) {
+        numeros.push(num);
+        break;
+      }
+      alert("Valor inválido. Por favor, digite um número.");
+    }
+  }
 
-document.writeln("<b>Vetor informado:</b><br>");
-for (let i = 0; i < n; i++) {
-  document.writeln(numeros[i] + "<br>");
-}
+  // Mostra o vetor informado
+  document.write("<b>Vetor informado:</b><br>");
+  numeros.forEach(num => document.write(num + "<br>"));
+})();
