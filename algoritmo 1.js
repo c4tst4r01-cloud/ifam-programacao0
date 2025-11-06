61330b62cfbb316b04a46582822c547a10194b35
c4tst4r01-cloud <c4tst4r01@gmail.com>
2025-11-06 01:37:46 -0400

    Update algoritmo 1.js

--- algoritmo 1.js
+++ algoritmo 1.js
@@ -1,12 +1,1 @@
 /*1 Faça um programa que lê n números inteiros do teclado e armazene em um vetor. Ao final imprima o vetor.*/
-let n = parseInt(prompt("Quantos números você quer digitar?"));
-let numeros = [];
-
-for (let i = 0; i < n; i++) {
-  numeros[i] = parseInt(prompt(`Digite o número ${i + 1}:`));
-}
-
-document.writeln("<b>Vetor informado:</b><br>");
-for (let i = 0; i < n; i++) {
-  document.writeln(numeros[i] + "<br>");
-}
