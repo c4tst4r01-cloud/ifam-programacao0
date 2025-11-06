0ea7fff467d4f4aff18c3c5d4748c3a8b03181fc
c4tst4r01-cloud <c4tst4r01@gmail.com>
2025-11-06 07:00:52 -0400

    Fix syntax errors and improve age calculation

---
+++ algoritmo5.js
@@ -0,0 +1,8 @@
+let n = parseInt(prompt("Digite sua idade" ))
+let soma =0
+for(let i=0; i<soma; i++){
+let idade = parseInt(prompt("Digite a idade idade"))
+ soma +=idade
+}
+ let media soma /n
+alert("resutado " + "media de idade"
